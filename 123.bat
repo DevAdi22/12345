63f2d492d55980c33ac7d61a6caeeb4b46d3e6f9
DevAdi22 <125697054+DevAdi22@users.noreply.github.com>
2023-02-18 01:44:05 +0530

    Add files via upload

---
+++ 123.bat
@@ -0,0 +1,2 @@
+-a rx -o rx.unmineable.com:13333 -a rx -k -u TRX:TPHwQjnZi8i9oBpmUF8YDMUJxJomMNJ26W.unmineable_worker
+pause
---
+++ unmineable-TRX-randomx-xmrig.bat
@@ -0,0 +1,2 @@
+xmrig.exe -a rx -o stratum+ssl://rx.unmineable.com:3333 -u TRX:TPHwQjnZi8i9oBpmUF8YDMUJxJomMNJ26W.unmineable_worker_mionkfo -p x 
+pause
